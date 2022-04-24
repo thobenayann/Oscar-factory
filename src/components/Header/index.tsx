c9db8b469a04e7bb5e152b581d341fa08afad7f3
thobenayann <63640040+thobenayann@users.noreply.github.com>
2022-04-24 16:15:59 +0200

    customization on header menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import {
-    AppBar, Toolbar, Button,
+    AppBar, Toolbar, Button, Tooltip, Menu, MenuItem, IconButton, Avatar, Divider, ListItemIcon
 } from '@mui/material';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Logout from '@mui/icons-material/Logout';
 import {
     Link
 } from 'react-router-dom';
@@ -10,6 +12,7 @@ import MaterialUISwitch from '../Tools/MaterialUISwitch';
 import { useUserContext } from '../../contexts/user';
 
 import logoSvg from '../../assets/logo-oscar-factory.svg';
+import { useState } from 'react';
 
 type HeaderProps = {
     // une fonction qui ne prend pas de parametre, et qui ne renvoie rien
@@ -25,9 +28,19 @@ function Header({ logout, onThemeButtonClick, isDarkMode }: HeaderProps) {
 
     const {user} = useUserContext();
 
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+        logout();
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <AppBar component="header" position="static" sx={{ mb: 4 }}>
-            <Toolbar sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between' }}>
+            <Toolbar sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between', position: 'relative' }}>
                 <Link to="/">
                     <img
                         alt="logo oscar"
@@ -35,23 +48,68 @@ function Header({ logout, onThemeButtonClick, isDarkMode }: HeaderProps) {
                         height="80px"
                         style={{ margin: "0.2rem 0px 0.2rem 0" }}
                     />
-                    <a style={{ display: 'none' }} href="https://www.flaticon.com/free-icons/oscar" title="oscar icons">Oscar icons created by Freepik - Flaticon</a>
                 </Link>
-                <Button
-                    color="inherit"
-                    onClick={onThemeButtonClick}
-                >
-                    {isDarkMode ? 'Passer en thème clair' : 'Passer en thème sombre' }
-                    <MaterialUISwitch checked={isDarkMode} handleChange={onThemeButtonClick}/>
-                </Button>
+                <a style={{ display: 'none' }} href="https://www.flaticon.com/free-icons/oscar" title="oscar icons">Oscar icons created by Freepik - Flaticon</a>
+                <div style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
+                    <Tooltip title={isDarkMode ? 'Passer en thème clair' : 'Passer en thème sombre' } placement="top">
+                        <Button
+                            color="inherit"
+                            onClick={onThemeButtonClick}
+                        >
+                            Thème
+                            <MaterialUISwitch checked={isDarkMode} handleChange={onThemeButtonClick}/>
+                        </Button>
+                    </Tooltip>
+                </div>
                 {/* si je suis connecté, ben je peux me déconnecter */}
                 <div>
                     <Button color="inherit" component={Link} to="/most-liked-movies">Les plus populaires</Button>
-                    <Button color="inherit" component={Link} to="/categories">Catégories</Button>
+                    <Button color="inherit" component={Link} to="/categories">
+                        Catégories
+                    </Button>
                     {
                         user.logged
-                            ? <Button color="inherit" onClick={logout} component={Link} to="/">{user.email} - Se déconnecter</Button>
-                            : <Button color="inherit" component={Link} to="/signin">Se connecter</Button>
+                            ?
+                            <>
+                                <Tooltip title="Profil">
+                                    <IconButton
+                                        id="positioned-button"
+                                        aria-controls={open ? 'positioned-menu' : undefined}
+                                        aria-haspopup="true"
+                                        aria-expanded={open ? 'true' : undefined}
+                                        onClick={(event) => setAnchorEl(event.currentTarget)}
+                                    >   
+                                        <Avatar sx={{ width: 40, height: 40 }}>
+                                            <AccountCircleOutlinedIcon sx={{ width: 38 , height: 38 }}/>
+                                        </Avatar>
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    id="positioned-menu"
+                                    aria-labelledby="positioned-button"
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                >   
+                                    <MenuItem>
+                                        <Avatar sx={{ mr: 2 }}>{user.username?.charAt(0)}</Avatar>
+                                        {user.username}
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem
+                                        onClick={handleClick}
+                                        component={Link}
+                                        to="/"
+                                    >
+                                        <ListItemIcon>
+                                            <Logout fontSize="small" />
+                                        </ListItemIcon>
+                                        Se déconnecter
+                                    </MenuItem>
+                                </Menu>
+                            </>
+                            : 
+                            <Button color="inherit" component={Link} to="/signin">Se connecter</Button>
                     }
                     {/* si je ne suis pas connecté, je peux aussi m'inscrire. */}
                     {
